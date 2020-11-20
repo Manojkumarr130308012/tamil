55a5acd3355d16e9b6efb36ac2dd64901aa939ee
santhiya <santhiya@neophrontech.com>
2020-11-20 13:19:21 +0530

    make it better

--- router/state.js
+++ router/state.js
@@ -23,5 +23,9 @@ router.put('/update', async (req, res) => {
 	const response = await stateController.update(req.query.id, req.body);
 	res.send(response);
 })
-
+router.get('/aggregation', async (req, res) =>{
+	let response = await stateController.aggregation();
+	res.send(response);
+	
+})
 module.exports = router;
