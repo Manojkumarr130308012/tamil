390e46d18a082e7562519c2f9cb63db795612fd5
santhiya <santhiya@neophrontech.com>
2020-12-07 15:04:06 +0530

    make it better

--- model/tracker.js
+++ model/tracker.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const trackerSchema = new mongoose.Schema({
     Event : {
-        // type: mongoose.Schema.ObjectId,
-        type: String,
+        type: mongoose.Schema.ObjectId,
         required: false
     }, 
     TrackName:{
