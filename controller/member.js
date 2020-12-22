edc5ad288fe76aabfda94885e03a6df4fabdc82e
santhiya <santhiya@neophrontech.com>
2020-12-22 12:53:17 +0530

    make it better

--- controller/member.js
+++ controller/member.js
@@ -80,7 +80,7 @@ class memberController{
 					Chapter: ObjectId(Chapter)
 				}
 			},
-			{ $group : { Chapter : ""+Chapter, count: { $sum: 1 } } },	{$lookup:
+			{ $group : { Chapter : Chapter, count: { $sum: 1 } } },	{$lookup:
 				{
 				  from: "countries",
 				  localField: "Country",
