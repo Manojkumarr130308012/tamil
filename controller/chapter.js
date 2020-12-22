bb99f31f644b99ec7c8d9b6bf0899107872ef946
santhiya <santhiya@neophrontech.com>
2020-12-22 15:21:24 +0530

    make it better

--- controller/chapter.js
+++ controller/chapter.js
@@ -92,13 +92,7 @@ as: "CityNamesDetails"
 }
 }]);
 
-let result=await memberSchema.aggregate( [	
-	{
-	$match: {
-		Chapter: ObjectId(response[0]._id)
-	}
-}
-]);
+
 // console.log("ddddddd",""+response[0]._id)
 
 //    let result = await memberSchema.find({'_id':response[0]._id});
