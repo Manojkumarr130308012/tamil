3718313004d9ec7ec00d0f50d72a62a183c6000c
santhiya <santhiya@neophrontech.com>
2021-01-11 14:18:29 +0530

    make it better

--- model/Board.js
+++ model/Board.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const boardSchema = new mongoose.Schema({
+    chapter : {
+        type: mongoose.Schema.ObjectId,
+        required: false
+    },
     designation: {
         type: String,
         required: true
