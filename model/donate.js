ff70974c73fdbd882503aa000a6b67a09ab0da2e
santhiya <santhiya@neophrontech.com>
2021-01-28 13:05:37 +0530

    make it better

--- model/donate.js
+++ model/donate.js
@@ -37,5 +37,17 @@ const donateSchema = new mongoose.Schema({
         type: String,
         required: false
     },
+    document1 : {
+        type: String,
+        required: false
+    },
+    document2 : {
+        type: String,
+        required: false
+    },
+    document3 : {
+        type: String,
+        required: false
+    },
 })
 module.exports = new mongoose.model('donate',donateSchema);
